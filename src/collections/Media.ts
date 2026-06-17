4515f91757c5bc162739af40c88fefba95cf8882
Simone <simone@threadworkdigital.com.au>
2026-06-18 08:44:38 +1000

    Regenerate import map for S3 storage plugin

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -40,8 +40,8 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: {
-    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    staticDir: path.resolve(dirname, '../../public/media'),
+    // DELETED Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
+    // DELETED staticDir: path.resolve(dirname, '../../public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
     imageSizes: [
