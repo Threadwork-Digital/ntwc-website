1b40a93fc64e3726e3f77c24e52c7408762b3361
Simone <simone@threadworkdigital.com.au>
2026-06-18 08:35:54 +1000

    Reinstate local staticDir from Media collection, use R2 storage adapter

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -40,8 +40,8 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: {
-    // DELETED Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    // DELETED staticDir: path.resolve(dirname, '../../public/media'),
+    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
+    staticDir: path.resolve(dirname, '../../public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
     imageSizes: [
